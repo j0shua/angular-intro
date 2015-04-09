e21c7ed204e8d41b822cc7be03401b1beddc5f04
j0shua <jgoldsmith@liveintent.com>
2015-04-09 13:53:04 -0400

    adding organized dir where controllers/services are broken out into separate files

---
+++ scripts/services.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('app')
+	.factory('PeopleService', function($timeout, $q){
+		var people = [
+			{'name': 'josh', 'word': 'awesome'}, 
+			{'name': 'vanessa', 'word': 'phenominal'},
+			{'name': 'jp', 'word': 'jpjpjppjpjpj'},
+			{'name': 'anton', 'word': 'anton anton anton'},
+			{'name': 'garreth', 'word': 'garreth garreth garreth'},
+			{'name': 'paul', 'word': 'paul paul paul'},
+			{'name': 'stan', 'word': 'stan stan stan'},
+			{'name': 'santiago', 'word': 'santiago santiago'},
+			{'name': 'tomas', 'word': 'tomas'},
+			{'name': 'ramiro', 'word': 'ramiro ramiro'},
+			{'name': 'julieta', 'word': 'julieta julieta'}
+		];
+
+		function getAll(){
+			var defer = $q.defer();
+
+			$timeout(function(){
+				defer.resolve(people);
+
+			}, 3000);
+
+			return defer.promise;
+		}
+
+
+		return {
+			getAll: getAll
+		};
+	})
