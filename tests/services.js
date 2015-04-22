0d61d3de5108a5bf95b78a928e2c81b5a1f64611
Aaron Tidwell <atidwell@aaron-tidwell-macbook-pro.local>
2015-04-22 10:19:05 -0400

    add httpbackend checks

--- tests/services.js
+++ tests/services.js
@@ -1,6 +1,6 @@
 describe("AdvertiserService", function() {
 	var AdvertiserService;
-	var httpBackend;
+	var $httpBackend;
 	var mockData = ['item1', 'item2'];
 
 	beforeEach(module('app'));
@@ -9,21 +9,27 @@ describe("AdvertiserService", function() {
 		//cache the service
 		AdvertiserService = _AdvertiserService_;
 		// Set up the mock http service responses
-		httpBackend = $injector.get('$httpBackend');
+		$httpBackend = $injector.get('$httpBackend');
 	}));
 
 	describe('get method', function() {
 		it('should make a request to /advertisers/ when get is called', function() {
 			//define the request we expect the service to make
-			httpBackend.whenGET('/advertisers/someId').respond(mockData);
+			$httpBackend.whenGET('/advertisers/someId').respond(mockData);
 
 			//call the method from the service
 			AdvertiserService.get('someId').then(function(data) {
 				//verify we got the data back we expected
 				expect(data).toEqual(mockData);
 			});
-			//reset the httpBackend for the next test
-			httpBackend.flush();
+			//reset the $httpBackend for the next test
+			$httpBackend.flush();
 		});
+	});
+
+	//verify that the http requests were resolved correctly after each test
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
 	});
 });
